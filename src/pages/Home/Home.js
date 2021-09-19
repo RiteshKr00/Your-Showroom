c31a22633901ffb5929c146c392a336b1b231efc
gauravsinhaweb <sinhagaaurav37@gmail.com>
2021-09-19 15:15:08 +0530

    commit

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,21 +1,41 @@
 import React from "react";
 import "./Home.css";
+import bmw from "../../assets/bmw.png";
+import ford from "../../assets/ford.png";
+import benz from "../../assets/benz.png";
+import rr from "../../assets/rr.png";
+
 function Home() {
   return (
     <>
-      <div className="car"></div>
-      <div>
-        <span class="scroll-btn">
-          <a href="#">
-            <span class="mouse">
-              <span></span>
+      <div className="" style={{ backgroundColor: "#C400FF" }}>
+        <div className="car"></div>
+        <div className="bg-gray-900">
+          <div>
+            <span class="scroll-btn">
+              <a href="#">
+                <span class="mouse">
+                  <span></span>
+                </span>
+              </a>
+              <p>scroll me</p>
             </span>
-          </a>
-          <p>scroll me</p>
-        </span>
-      </div>
-      <div className="my-40 flex flex-wrap justify-around">
-        <div className="h-80 w-80 border border-black rounded"></div>
+          </div>
+          <div className="py-40 flex flex-wrap justify-around pt-64">
+            <div className="h-80 w-80 rounded cursor-pointer flex items-center shadow-2xl border border-opacity-10 hover:border-opacity-30">
+              <img src={benz} alt="benz" style={{ transform: "scaleX(-1)" }} />
+            </div>{" "}
+            <div className="h-80 w-80 rounded cursor-pointer flex items-center shadow-2xl border border-opacity-10 hover:border-opacity-30">
+              <img src={bmw} alt="bmw" />
+            </div>{" "}
+            <div className="h-80 w-80 rounded cursor-pointer flex items-center shadow-2xl border border-opacity-10 hover:border-opacity-30">
+              <img src={rr} alt="rr" />
+            </div>
+            <div className="h-80 w-80 rounded cursor-pointer flex items-center shadow-2xl border border-opacity-10 hover:border-opacity-30">
+              <img src={ford} alt="ford" />
+            </div>{" "}
+          </div>
+        </div>
       </div>
     </>
   );
