b057b174a2647c336ba637085d9c0555858e857d
Your Name <hr5359@srmist.edu.in>
2021-09-19 16:39:49 +0530

    🐛 Paint bug fixed

--- src/components/3DModel/Ford3D.js
+++ src/components/3DModel/Ford3D.js
@@ -117,7 +117,7 @@ export default function Perseverance() {
                         </Suspense>
                         <OrbitControls />
                     </Canvas>
-                    <Link to="/fordColor" className="md:w-1/3">
+                    <Link to="/paint" className="md:w-1/3">
                         <button
                             className="bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 w-64 my-4 ml-8 py-4 text-white active:bg-pink-600 font-bold uppercase text-sm px-6 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 ease-linear transition-all duration-150 font-fontVollkorn"
                             type="button"
